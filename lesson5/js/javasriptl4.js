2bf67d2a42b55742d2794a1d70b10e32f942404e
WDD 230  Test student Account <jjuanlozanochoa1993@gmail.com>
2021-05-23 20:00:37 -0500

    update

---
+++ lesson5/js/javasriptl4.js
@@ -0,0 +1,17 @@
+let year = new Date ();
+document.getElementById("Ayear").innerHTML = year.getFullYear();
+
+var jdate = document.lastModified;
+document.getElementById("last-update").innerHTML = jdate;
+
+/* -----------navigation responsive------------*/
+const hambutton = document.querySelector('.pmenu');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+
+
