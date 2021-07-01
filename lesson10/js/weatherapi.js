1f49f0e597182abbe95b6bd633f7f2f688177658
WDD 230  Test student Account <jjuanlozanochoa1993@gmail.com>
2021-06-30 23:13:30 -0500

    update

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -5,11 +5,15 @@ fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
+    
+  
+    document.getElementById('humidity-preston').textContent = jsObject.main.humidity.toFixed(0);
+    document.getElementById('wind-speed').textContent = jsObject.wind.speed.toFixed(0);
     document.getElementById('current-temp').textContent = jsObject.main.temp.toFixed(0);
-    document.getElementById('current-temp').textContent = jsObject.main.temp.toFixed(0);
+    
     const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
 const desc = jsObject.weather[0].description;  // note how we reference the weather array
 document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
 document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
 document.getElementById('icon').setAttribute('alt', desc);
-  });
+  });  
