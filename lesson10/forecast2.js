d69c14c53331412040f97ec7b46550c10c2c3d7e
WDD 230  Test student Account <jjuanlozanochoa1993@gmail.com>
2021-07-01 23:56:25 -0500

    update

---
+++ lesson10/forecast2.js
@@ -0,0 +1,20 @@
+let appiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&unit=imperial&appid=0e326d95be48b5bfae6bde59d73f8694";
+fetch(appiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    let day = 0;
+    const dayofweek = ['Sun','mon','tue','Wed','Thu','Fri','Sat'];
+    const fiveDayForecast = jsObject.list.filter( forecast => forecast.dt_txt.includes('12:00:00'));
+    console.log(fiveDayForecast);
+    
+    
+    fiveDayForecast.forEach( x => {
+        let d = new Date(x.dt_txt);
+        console.log(d);
+        document.getElementById(`dayofweek${day+1}`).textContent = dayofweek[d.getDay()];
+        document.getElementById(`forecast${day+1}`).textContent = x.main.temp; 
+        day++
+    }); 
+
+  });  
