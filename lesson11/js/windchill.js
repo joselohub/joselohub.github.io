babf6159389616b7f822218c6bb8c5500ce920f3
WDD 230  Test student Account <jjuanlozanochoa1993@gmail.com>
2021-07-06 06:58:36 -0500

    update

--- lesson11/js/windchill.js
+++ lesson11/js/windchill.js
@@ -1,3 +1,3 @@
 let wchill = document.getElementById("wind-chill").innerHTML;
 let tm = document.getElementById("temperature").innerHTML;
-let wspeed = document.getElementById("wind-speed")
+let wspeed = document.getElementById("wind-speed");
